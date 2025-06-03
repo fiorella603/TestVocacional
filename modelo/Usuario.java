c33a2e7a04a8edbec72e1d5b027d572b89cbedda
Geno5516 <genovevagillon@gmail.com>
2025-06-03 11:49:12 -0300

    Subimos modelo de dominio

---
+++ modelo/Usuario.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Us
+ */
+public class Usuario {
+    private int id;
+    private String email;
+    private String nombreUsuario;
+    private int dni;
+    
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, int dni, String email, String nombreUsuario) {
+        this.id = id;
+        this.dni = dni;
+        this.email = email;
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+   
+     
+    
+    
+}
